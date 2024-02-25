33d0efb03d66ff80520c0068a90524772cc21a55
Mason <wuichyen@gmail.com>
2024-02-25 22:39:20 +0100

    homepage

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Blog from './components/Blog';
 import NewPostForm from './components/NewPostForm';
 import Navbar from './components/Navbar';
+import backgroundImage from './img/wixref.webp';
+
+const appStyle = {
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundSize: 'cover',
+  height: '100vh', // Adjust the height as needed
+  // Add other background-related styles if necessary
+};
 
 const App = () => {
   return (
-    <div>
+    <div style={appStyle}>
       {/* Other components or layout */}
       <Blog />
       <NewPostForm />
