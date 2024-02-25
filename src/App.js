ce9584143cb0e3ea27582dfb0f927f0288f363bc
Mason <wuichyen@gmail.com>
2024-02-25 22:31:04 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // App.js
 
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Blog from './components/Blog';
 import NewPostForm from './components/NewPostForm';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
